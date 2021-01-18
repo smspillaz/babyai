169a712108dc6f4dc5dd08b29c718ae78404c1aa
Sam Spilsbury <smspillaz@gmail.com>
2021-01-18 15:20:55 +0200

    setup: Use find_packages() instead of explicitly listing packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='babyai',
     version='1.1',
     license='BSD 3-clause',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    packages=['babyai', 'babyai.levels', 'babyai.utils'],
+    packages=find_packages(),
     install_requires=[
         'gym>=0.9.6',
         'numpy==1.15.4', # Temporary: fix numpy version because of bug introduced in 1.16
